ad8c804a9c2ea3354bc13dc6222be3376860e182
pedrogabrieloss <pedro.gabriel.eng@outlook.com>
2023-01-23 17:08:41 -0300

    Inicio da classe Contrato

---
+++ sistemaPagamentoTransporte/src/sistemaPagamentoTransporte/Contrato.java
@@ -0,0 +1,17 @@
+package sistemaPagamentoTransporte;
+
+public class Contrato {
+
+	
+//****************************************** ATRIBUTOS ******************************************//
+	private String nomeEmpresa, tipoVeiculo;
+	private int qntdVeiculos;
+	private double numContrato, valorContrato;
+	// private Date dataAssinatura, dataInicio, dataVencimento;
+	
+//****************************************** CONSTRUTORES ******************************************//
+	
+//**************************************** SETTERS & GETTERS ****************************************//
+
+//******************************************** MÉTODOS ********************************************//
+}
