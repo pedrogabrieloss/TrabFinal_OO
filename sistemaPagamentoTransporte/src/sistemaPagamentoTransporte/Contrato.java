97244d6a9f24c88820f9632d9fe1ef5d667f47f1
pedrogabrieloss <pedro.gabriel.eng@outlook.com>
2023-01-23 17:40:38 -0300

    Inicio da classe Contrato - pt3

--- sistemaPagamentoTransporte/src/sistemaPagamentoTransporte/Contrato.java
+++ sistemaPagamentoTransporte/src/sistemaPagamentoTransporte/Contrato.java
@@ -1,5 +1,7 @@
 package sistemaPagamentoTransporte;
 
+//import java.util.*;
+
 public class Contrato {
 
 	
@@ -7,11 +9,18 @@ public class Contrato {
 	private String nomeEmpresa, tipoVeiculo;
 	private int qntdVeiculos;
 	private double numContrato, valorContrato;
-	// private Date dataAssinatura, dataInicio, dataVencimento;
+//	private Date dataAssinatura, dataInicio, dataVencimento;
+// Se for muito complicado manipular as variáveis com Date, usar int e quebrar em dd, mm e ano.
+	
 	
 //****************************************** CONSTRUTORES ******************************************//
 	
-//**************************************** SETTERS & GETTERS ****************************************//
+	Contrato(String nomeEmpresa, String tipoVeiculo, int qntdVeiculos, double numContrato,
+			double valorContrato) {
+		
+	}
+	
+//*************************************** SETTERS & GETTERS ***************************************//
 
 //******************************************** MÉTODOS ********************************************//
 }
