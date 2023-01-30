e374405d6417544b389889c3aa4805a107f9645a
Igorabreueng <igoralves.ab@gmail.com>
2023-01-30 18:45:52 -0300

    Trying to commit

---
+++ untitled/src/sistemaPagamentoTransporte/Banco.java
@@ -0,0 +1,43 @@
+package sistemaPagamentoTransporte;
+
+public class Banco {
+    private String nome;
+    private int codigoBoleto;
+
+    Banco(String nome, int codigoBoleto){
+        this.nome = nome;
+        this.codigoBoleto = codigoBoleto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigoBoleto() {
+        return codigoBoleto;
+    }
+
+    public void setCodigoBoleto(int codigoBoleto) {
+        this.codigoBoleto = codigoBoleto;
+    }
+
+    //******************************************** MÉTODOS ********************************************//
+
+    public comprovantedePagamento(); {
+
+    }
+    public verificarSaldo();{
+
+    }
+    public receberPagamentos();{
+
+    }
+    public alterarSALDO(); {
+
+    }
+
+}
