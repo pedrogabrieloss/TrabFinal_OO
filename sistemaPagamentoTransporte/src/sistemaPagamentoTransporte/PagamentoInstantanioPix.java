c6aa582ad7e44e2a85f656ce9f3daf31c90586f2
pedrogabrieloss <pedro.gabriel.eng@outlook.com>
2023-02-10 10:48:33 -0300

    Commit das classes do Igor

---
+++ sistemaPagamentoTransporte/src/sistemaPagamentoTransporte/PagamentoInstantanioPix.java
@@ -0,0 +1,55 @@
+package sistemaPagamentoTransporte;
+
+public class PagamentoInstantanioPix {
+	
+	//****************************************** ATRIBUTOS ******************************************//
+	
+	private String chavePix; 
+	private String nome;
+	private float valor; 
+	private String tipoPix;
+	
+	//***************************************** CONSTRUTORES ******************************************//	
+	
+	public String getChavePix() {
+		return chavePix;
+	}
+	public PagamentoInstantanioPix(String chavePix, String nome, float valor, String tipoPix) {
+		super();
+		this.chavePix = chavePix;
+		this.nome = nome;
+		this.valor = valor;
+		this.tipoPix = tipoPix;
+	}	
+		
+	public PagamentoInstantanioPix() {
+		super();
+	}
+	//*************************************** SETTERS & GETTERS ***************************************//
+
+	
+		
+	public void setChavePix(String chavePix) {
+		this.chavePix = chavePix;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public float getValor() {
+		return valor;
+	}
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+	public String getTipoPix() {
+		return tipoPix;
+	}
+	public void setTipoPix(String tipoPix) {
+		this.tipoPix = tipoPix;
+	}
+		
+	
+}
